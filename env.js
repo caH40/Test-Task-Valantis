dc5f39946f7c1d56b5366c15ab07be29d52024b1
Alexander Berezhnev <freelancerbav@gmail.com>
2024-03-09 22:39:35 +0300

    preparing for deployment on github pages

---
+++ env.js
@@ -0,0 +1,3 @@
+// для github Pages
+export const VITE_API_PASSWORD = 'Valantis';
+export const VITE_API_SERVER = 'http://api.valantis.store:40000/';
--- src/config/environment.js
+++ src/config/environment.js
@@ -1,3 +1,4 @@
-export const apiPassword = import.meta.env.VITE_API_PASSWORD;
-export const apiServer = import.meta.env.VITE_API_SERVER;
-export const isDevelopmentMode = import.meta.env.VITE_NODE_ENV === 'development';
+import { VITE_API_PASSWORD, VITE_API_SERVER } from '../../env';
+
+export const apiPassword = VITE_API_PASSWORD;
+export const apiServer = VITE_API_SERVER;
