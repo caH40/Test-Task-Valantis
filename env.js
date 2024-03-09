b5d595cef261d463b5a8c4883c79773298d4cde8
Alexander Berezhnev <freelancerbav@gmail.com>
2024-03-09 23:47:21 +0300

    working version for githubpages

--- env.js
+++ env.js
@@ -1,3 +1,3 @@
 // для github Pages
 export const VITE_API_PASSWORD = 'Valantis';
-export const VITE_API_SERVER = 'http://api.valantis.store:40000/';
+export const VITE_API_SERVER = 'https://api.valantis.store:41000/';
